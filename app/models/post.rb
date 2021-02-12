365fe42766a0804f53c41018ac1cc47795e7dbe3
George <george.gbenle@outlook.com>
2021-02-12 16:56:25 +0000

    Set up post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     validates :title, :length => { :minimum => 5 }, presence: true
     validates :body, :length => { :minimum => 5 }, presence: true
+
+    belongs_to :user
 end
